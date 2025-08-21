32d385ab567af18fadc00cdd693d62cac7efc902
elroi773 <s2457@e-mirim.hs.kr>
2025-08-21 10:36:01 +0900

    정렬 맞추기

--- join.py
+++ join.py
@@ -14,34 +14,32 @@ canvas.create_image(0, 0, anchor="nw", image=bg_img)
 id_var = tk.StringVar()
 pw_var = tk.StringVar()
 
-# 아이디 입력창 (투명 느낌)
+# 아이디 입력창 (배경 색과 맞추기)
 id_entry = tk.Entry(root, textvariable=id_var,
                     font=("Arial", 14),
-                    fg="black", bg="#dff",   # 배경 이미지 색상과 비슷하게
+                    fg="black", bg="#bdefff",  # 배경 버튼 색상 직접 맞추기
                     bd=0, highlightthickness=0,
                     justify="center", insertbackground="black")
-id_entry.place(x=230, y=150, width=140, height=25)
+id_entry.place(x=180, y=200, width=200, height=30)
 
 # 비밀번호 입력창
 pw_entry = tk.Entry(root, textvariable=pw_var,
                     font=("Arial", 14),
                     show="*",
-                    fg="black", bg="#dff",
+                    fg="black", bg="#bdefff",
                     bd=0, highlightthickness=0,
                     justify="center", insertbackground="black")
-pw_entry.place(x=230, y=195, width=140, height=25)
+pw_entry.place(x=180, y=160, width=200, height=30)
 
-# 버튼 이미지
-submit_img = PhotoImage(file="./img/join_submit.png")   # 제출 버튼 이미지
+# 제출 버튼 이미지
+submit_img = PhotoImage(file="./img/join_submit.png")
 
-# 버튼 동작
 def submit_action():
     print("아이디:", id_var.get())
     print("비밀번호:", pw_var.get())
 
-# 제출 버튼
 submit_btn = tk.Button(root, image=submit_img, command=submit_action,
                        bd=0, highlightthickness=0, relief="flat", cursor="hand2")
-submit_btn.place(x=230, y=240)
+submit_btn.place(x=180, y=240)
 
 root.mainloop()
