6a9d437adbdb2441ce0d54f792bb2cdefa659388
김이레 <165885725+elroi773@users.noreply.github.com>
2025-11-29 20:15:49 +0900

    Remove hardcoded database password
    Removed hardcoded database password for security.

--- db.py
+++ db.py
@@ -4,7 +4,6 @@ def connect_db():
     return pymysql.connect(
         host="localhost",
         user="root",   
-        password="Mysql4344!",   
         database="bePerson",
         charset="utf8mb4",
         cursorclass=pymysql.cursors.DictCursor
